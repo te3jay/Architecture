1060f82989244d9c4a02047771681a976a9b563c
Tjeu Roefs <tjeuroefs@hotmail.com>
2019-04-19 15:46:26 +0200

    photos

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -2,3 +2,24 @@ console.log('%c Hello from /scripts/script.js ', 'background: lemonchiffon; bord
 console.log('%c Welcome to tris-webpack-boilerplate! ', 'background: lavenderblush; border: 1px solid #fff');
 
 // add custom js below
+
+// Initialize and add the map
+$(document).ready(function(){
+
+    function initMap() {
+        // The location of Uluru
+        var uluru = {lat: -25.344, lng: 131.036};
+        // The map, centered at Uluru
+        var map = new google.maps.Map(
+            document.getElementById('map'), {zoom: 4, center: uluru});
+        // The marker, positioned at Uluru
+        var marker = new google.maps.Marker({position: uluru, map: map});
+      }
+
+})
+
+
+
+
+
+  
