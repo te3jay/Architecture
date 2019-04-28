3b7d1b15aad3ef6f0b7bc3383a10155f84c86d6d
Tjeu Roefs <tjeuroefs@hotmail.com>
2019-04-28 21:52:41 +0200

    styling

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 require('offline-plugin/runtime').install();
+import './images/Gallery/scott-webb-176434.jpg';
+//import './images/Photo2.png';
+//import './images/Photo3.png';
+//import './images/Photo4.png';
 
 
 
-import './index.html';
 import './index.scss';
 import './scripts/script';
+import './scripts/materialize';
 //import './scripts/bootstrap.bundle';
-//import './scripts/masonry.pkgd.min.js';
+import './scripts/masonry.pkgd.min.js';
+import './index.html';
+
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -4,6 +4,39 @@ console.log('%c Welcome to tris-webpack-boilerplate! ', 'background: lavenderblu
 // add custom js below
 
 
+$(document).ready(function() {
+
+    var platform = new H.service.Platform({
+        'app_id': 'fKEC7OAHBP6lBqaE85xZ',
+        'app_code': 'sDmZ1ti4EQ1uH4QmTrPtgQ'
+      });
+
+      var defaultLayers = platform.createDefaultLayers();
+
+// Instantiate (and display) a map object:
+var map = new H.Map(
+  document.getElementById('mapContainer'),
+  defaultLayers.normal.map,
+  {
+    zoom: 13,
+    center: { lat: 51.219448, lng: 4.402464 }
+  });
+
+  var ui = H.ui.UI.createDefault(map, defaultLayers);
+
+    $(".burger").click(function(){
+        $(".burger").toggleclass("open")
+    })
+
+    $('.grid').masonry({
+        itemSelector: '.grid-item',
+        columnWidth: '.grid-sizer',
+        gutter: 15,
+        percentPosition: true
+    });
+  
+
+});  
 
 
 
